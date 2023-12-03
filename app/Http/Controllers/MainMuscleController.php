b152bb4c122c71c467b3d079be520ccb8db76fe4
Luferva <lfrv2005@gmail.com>
2023-12-03 20:44:45 -0300

    criado CRUD dos músculos principal

---
+++ app/Http/Controllers/MainMuscleController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MainMuscle;
+use App\Http\Requests\StoreMainMuscleRequest;
+use App\Http\Requests\UpdateMainMuscleRequest;
+
+class MainMuscleController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StoreMainMuscleRequest $request)
+    {
+        $result = MainMuscle::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'success' => $result,
+            'message' => $result ? 'Músculo principal criado com sucesso' : 'Erro ao criar músculo principal'
+        ]);
+    }
+
+    public function update(UpdateMainMuscleRequest $request)
+    {
+        $result = MainMuscle::where('id', $request->id)->update([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'success' => $result,
+            'message' => $result ? 'Músculo principal atualizado com sucesso' : 'Erro ao atualizar músculo principal'
+        ]);
+    }
+
+    public function destroy(MainMuscle $mainMuscle)
+    {
+        $result = $mainMuscle->delete();
+
+        return response()->json([
+            'success' => $result,
+            'message' => $result ? 'Músculo principal excluído com sucesso' : 'Erro ao excluir másculo principal'
+        ]);
+    }
+}
